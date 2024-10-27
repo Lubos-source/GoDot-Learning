fb9ca3fb0f8abd7c0efa6c32a8d8ddbd2be7c60f
Lubos-source <luboshorky2@seznam.cz>
2024-10-27 19:47:30 +0100

    First learning project

---
+++ animated_player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+@onready var _animated_sprite = $AnimatedSprite2D
+
+var speed = 200  # Nastav rychlost postavy
+var direction = Vector2.ZERO
+
+func _process(_delta):
+	direction = Input.get_vector("walk_left", "walk_right", "walk_up", "walk_down")
+	
+	velocity = direction * speed
+	
+	if Input.is_action_pressed("walk_right"):
+		_animated_sprite.flip_h = false
+		_animated_sprite.play("walk")
+	elif Input.is_action_pressed("walk_left"):
+		_animated_sprite.flip_h = true
+		_animated_sprite.play("walk")
+	elif (Input.is_action_pressed("walk_up") or Input.is_action_pressed("walk_down")):
+		_animated_sprite.play("walk")
+	else:
+		_animated_sprite.stop()
+		
+	if Input.is_action_just_pressed("ui_select"):
+		print("Klávesa stisknuta")
+		
+	move_and_slide()
